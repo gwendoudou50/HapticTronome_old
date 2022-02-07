bb4f9302e944bd3a0d5e52d4d01d6ea013e1232d
gwendoudou50 <gwendal.aube@icloud.com>
2022-02-07 20:45:10 +0100

    Initial Commit

---
+++ HapticTronome/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HapticTronome
+//
+//  Created by Gwendal Aubé on 07/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HapticTronome/HapticTronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticTronomeApp.swift
+//  HapticTronome
+//
+//  Created by Gwendal Aubé on 07/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct HapticTronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
