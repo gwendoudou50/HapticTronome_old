b74a95481556ba899aa0a8a16cb8545c68befa7f
gwendoudou50 <gwendal.aube@icloud.com>
2022-02-08 23:48:08 +0100

    Pointer created and positioned on the tempo button

--- HapticTronome/Views/TempoButton.swift
+++ HapticTronome/Views/TempoButton.swift
@@ -9,13 +9,60 @@ import SwiftUI
 
 struct TempoButton: View {
     var body: some View {
-        Circle()
-            .fill(.red)
+        
+        ZStack {
+            
+            //Circle
+            Image(systemName: "circle")
+                .foregroundColor(.red)
+                .frame(width: 166, height: 166)
+                .background(
+                    ZStack {
+                        Color.red
+                        
+                        Circle()
+                            .foregroundColor(.black).opacity(0.25)
+                            .blur(radius: 5)
+                            .offset(x: 0, y: -8)
+                        
+                        Circle()
+                            .foregroundColor(.black).opacity(0.25)
+                            .blur(radius: 3)
+                            .offset(x: 8, y: 8)
+                        
+                        Circle()
+                            .foregroundColor(.red)
+                            .padding(2)
+                            .blur(radius: 2)
+                    }
+                )
+            .clipShape(Circle())
+            
+            //Pointer
+            Pointer()
+                .offset(x: 0, y: -55.5)
+        }
     }
 }
 
 struct TempoButton_Previews: PreviewProvider {
     static var previews: some View {
         TempoButton()
+    }
+}
+
+struct Pointer: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 2)
+                .foregroundColor(.black)
+                .frame(width: 8, height: 55)
+            
+            Rectangle()
+                .foregroundColor(.white)
+                .blur(radius: 1)
+                .clipShape(Rectangle())
+                .frame(width: 5, height: 52)
+        }
     }
 }
