00b1fbac13fae45ee821b64e020544896f6fa857
gwendoudou50 <gwendal.aube@icloud.com>
2022-02-11 22:23:08 +0100

    TUTO: function onDrag done

--- HapticTronome/Models/HomeViewModel.swift
+++ HapticTronome/Models/HomeViewModel.swift
@@ -12,5 +12,36 @@ class HomeViewModel: ObservableObject {
     @Published var progress: CGFloat = 0
     @Published var angle: Double = 0
     
-
+    func onDrag(value: DragGesture.Value) {
+        
+        // calculating radians...
+        
+        let vector  = CGVector(dx: value.location.x, dy: value.location.y)
+        
+        // since atan2 will give from -180 to 180
+        // eliminating drag gesture size
+        // size = 55 => Radius = 27.5...
+        let radians = atan2(vector.dy - 27.5, vector.dx - 27.5)
+        
+        // converting to angle...
+        
+        var angle = radians * 180 / .pi
+        
+        // simple technique for 0 to 360
+        
+        // eg = 360 - 176 = 184...
+        if angle < 0 {
+            angle = 360 + angle
+        }
+        
+        withAnimation(Animation.linear(duration: 0.15)) {
+            
+            // progress...
+            let progress = angle / 360
+            self.progress = progress
+            self.angle = Double(angle)
+            
+        }
+        
+    }
 }
